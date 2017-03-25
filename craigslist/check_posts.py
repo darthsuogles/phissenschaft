917c67bacce09d75cfd15062d2dcf4ba20b3ebe4
Philip Yang <phi@cs.umd.edu>
2017-03-25 10:18:05 -0700

    craigslist crawl

--- craigslist/check_posts.py
+++ craigslist/check_posts.py
@@ -1,7 +1,9 @@
 import pandas as pd
+# Install the [speedup] version with fast Levenstein distance
 from fuzzywuzzy import fuzz, process as fuzproc
 
 df = pd.read_csv('craigslist_posts.df.csv').sort_values('listing_price')
 #!+TODO: analyze text and send requests o ones that are interesting
 
 fuzproc.extract('mateo', df.title, limit=10)
+fuzproc.extract('fitness', df.title, limit=10)
