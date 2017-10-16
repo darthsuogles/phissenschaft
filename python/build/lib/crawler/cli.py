44bf7168a30c727c0ef85e3d2f3345049be97203
Philip Yang <phissenschaft@gmail.com>
2017-10-15 19:08:13 -0700

    delete ignored files from cache

--- python/build/lib/crawler/cli.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', '-c', help='prepare configuration')
-    args = parser.parse_args()
-    print(args)
