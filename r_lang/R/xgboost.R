ecc52479dc3e52600bddf72906deb0b2ffa09ee6
Philip Yang <phissenschaft@gmail.com>
2017-12-04 21:49:30 -0800

    batch update

---
+++ r_lang/R/xgboost.R
@@ -0,0 +1,20 @@
+source("../import_lib.R")
+import.pkgs('plyr', 'glmnet', 'caret', 'ggplot2', 'prophet', 'xgboost')
+
+## load data
+data(agaricus.train, package='xgboost')
+data(agaricus.test, package='xgboost')
+train <- agaricus.train
+test <- agaricus.test
+
+## fit model
+bst <- xgboost(data = train$data,
+              label = train$label,
+              max.depth = 2,
+              eta = 1,
+              nround = 2,
+              nthread = 2,
+              objective = "binary:logistic")
+
+## predict
+pred <- predict(bst, test$data)
