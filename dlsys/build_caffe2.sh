375285f4672e5e758bd0f2b401b3d61389e9018d
Philip Yang <darthsuogles@gmail.com>
2018-02-14 20:37:48 -0800

    cycle gan

--- dlsys/build_caffe2.sh
+++ dlsys/build_caffe2.sh
@@ -23,8 +23,28 @@ function config_cmake {
           ..
 }
 
-pushd "${_bsd_}/caffe2"; mkdir -p build-tree && pushd $_
-config_cmake
-make clean
-make -j8
-popd; popd
+function build_caffe2_from_source {
+    pushd "${_bsd_}/caffe2"; mkdir -p build-tree && pushd $_
+    config_cmake
+    make clean
+    make -j8
+    popd; popd
+}
+
+function get_caffe2_docker {
+    # Need the full version with opencv
+    docker pull caffe2ai/caffe2:c2v0.8.0.cpu.full.ubuntu16.04
+
+    local script_fname="${_bsd_}/with_caffe2_env.sh"
+    cat << _SCRIPT_EOF_ | tee "${script_fname}"
+#!/bin/bash
+
+docker run -it \
+       -v "${_bsd_}":/workspace -w /workspace \
+       caffe2ai/caffe2:c2v0.8.0.cpu.full.ubuntu16.04 \
+       /bin/bash
+_SCRIPT_EOF_
+    chmod +x "${script_fname}"
+}
+
+get_caffe2_docker
