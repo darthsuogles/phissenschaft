87fcba292a86612ec83484ab6895fd71262a417c
Philip Yang <darthsuogles@gmail.com>
2019-01-27 20:05:31 -0800

    batch update

--- dlsys/hw_resnet.py
+++ dlsys/hw_resnet.py
@@ -2,19 +2,87 @@
 """
 import tensorflow as tf
 
+import numpy as np
+
 from collections import namedtuple
+from dlsys.pixsrl import show_graph
 
-slim = tf.contrib.slim
-layers = tf.keras.layers
+L = tf.keras.layers
+Seq = tf.keras.Sequential
+print(tf.keras.__version__)
 
-from tensorflow.contrib.slim import nets
-from tensorflow.contrib.slim.nets import resnet_utils
 
-resnet_arg_scope = resnet_utils.resnet_arg_scope
+def conv2d(out_channels, kernel_size, strides=1):
+    return L.Conv2D(
+        filters=out_channels,
+        kernel_size=(7, 7),
+        strides=(2, 2),
+        padding="same",
+        data_format="channels_first")
 
+
+def conv2d_bn(out_channels, kernel_size, strides=1):
+    return Seq([
+        conv2d(out_channels, kernel_size, strides),
+        L.BatchNormalization(),
+        L.ReLU(),
+    ])
+
+
+def linear_cls(num_classes):
+    return Seq([
+        L.GlobalAveragePooling2D(data_format="channels_first"),
+        L.Dense(4, activation="relu"),
+        L.Softmax(),
+    ])
+
+
+def build_model():
+    return Seq([
+        conv2d_bn(32, kernel_size=7, strides=3),
+        conv2d_bn(64, kernel_size=3),
+        linear_cls(4),
+    ])
+
+try: print(iss)
+except: iss = tf.InteractiveSession()
+
+# sess.run(tf.global_variables_initializer())
+# ss = sess.run(
+#     probs, feed_dict={input_tensor: np.random.randn(1, 3, 80, 80)})
+
+graph = tf.Graph()
+with tf.Session(graph=graph) as sess:
+    model = build_model()
+    model.compile(
+        optimizer="rmsprop",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"])
+
+    train_data = np.random.randn(32, 3, 80, 80)
+    train_labels = np.random.randint(4, size=(32, 1))
+    model.fit(train_data, train_labels, batch_size=4, epochs=10)
+
+show_graph(graph)
 
 class Block(namedtuple("Block", ["scope", "unit_fn", "args"])):
-    pass
+    """A named tuple describing a ResNet block.
+      Its parts are:
+        scope: The scope of the `Block`.
+        unit_fn: The ResNet unit function which takes as input a `Tensor` and
+          returns another `Tensor` with the output of the ResNet unit.
+        args: A list of length equal to the number of units in the `Block`. The list
+          contains one (depth, depth_bottleneck, stride) tuple for each unit in the
+          block to serve as argument to unit_fn.
+      """
+
+
+class BlockSpec(
+        namedtuple("BlockSpec", ["depth", "depth_bottleneck", "stride"])):
+    """ A list of length equal to the number of units in the `Block`.
+        The list contains one (depth, depth_bottleneck, stride) tuple for
+        each unit in the block to serve as argument to unit_fn.
+    """
 
 
 def conv2d_same(inputs: tf.Tensor,
@@ -25,7 +93,7 @@ def conv2d_same(inputs: tf.Tensor,
                 data_format='channels_first',
                 scope=None):
     if 1 == stride:
-        conv = layers.Conv2D(
+        conv_impl = L.Conv2D(
             filters=num_outputs,
             kernel_size=kernel_size,
             stride=stride,
@@ -44,7 +112,7 @@ def conv2d_same(inputs: tf.Tensor,
         inputs = tf.pad(
             inputs,
             [[0, 0], [pad_begin, pad_end], [pad_begin, pad_end], [0, 0]])
-        conv = layers.Conv2D(
+        conv_impl = L.Conv2D(
             filters=num_outputs,
             kernel_size=kernel_size,
             stride=stride,
@@ -52,7 +120,7 @@ def conv2d_same(inputs: tf.Tensor,
             padding='valid',
             data_format=data_format)
 
-    return conv(inputs)
+    return conv_impl(inputs)
 
 
 def stack_blocks_dense(inputs: tf.Tensor,
